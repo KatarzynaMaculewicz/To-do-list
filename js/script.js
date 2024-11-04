57feb4f8bba428e934251f0c00e4105788884f7b
Katarzyna Maculewicz <qla.katarzyna@gmail.com>
2024-11-04 22:54:52 +0000

    script.js functions changed

--- js/script.js
+++ js/script.js
@@ -1,21 +1,24 @@
 {
-  const tasks = [];
+  let tasks = [];
+
+  let hideDoneTasks = false;
 
   const addNewTask = (newTaskContent) => {
-    tasks.push({
-      content: newTaskContent,
-    });
-
+    tasks = [...tasks, { content: newTaskContent }];
     render();
   };
 
   const removeTask = (taskIndex) => {
-    tasks.splice(taskIndex, 1);
+    tasks = [...tasks.slice(0, taskIndex), ...tasks.slice(taskIndex + 1)];
     render();
   };
 
   const toggleTaskDone = (taskIndex) => {
-    tasks[taskIndex].done = !tasks[taskIndex].done;
+    tasks = [
+      ...tasks.slice(0, taskIndex),
+      { ...tasks[taskIndex], done: !tasks[taskIndex].done },
+      ...tasks.slice(taskIndex + 1),
+    ];
     render();
   };
 
@@ -37,7 +40,7 @@
     });
   };
 
-  const render = () => {
+  const renderTasks = () => {
     let htmlString = "";
 
     for (const task of tasks) {
@@ -58,6 +61,13 @@
 
     document.querySelector(".js-tasks").innerHTML = htmlString;
     bindEvents();
+  };
+
+  const renderButtons = () => {};
+
+  const render = () => {
+    renderTasks();
+    renderButtons();
   };
 
   const onFormSubmit = (event) => {
